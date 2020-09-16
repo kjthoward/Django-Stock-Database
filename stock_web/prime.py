d81e50bde48c2d22d004e2d9bb860d8e3126d08a
Kieran Howard <kieran.howard@ouh.nhs.uk>
2020-09-16 11:40:06 +0100

    Added new groups to priming script

--- stock_web/prime.py
+++ stock_web/prime.py
@@ -4,14 +4,14 @@ from .models import ForceReset, Suppliers, Reagents, Internal, Validation, Recip
 
 def PRIME():
     with transaction.atomic():
-        User_Mod = Group.objects.create(name="User_Mod")
-        perms=['13','14','16','8','5','6','9','10','12']
-        for p in perms:
-            User_Mod.permissions.add(p)
-        Non_SU_Admin = Group.objects.create(name="Non_SU_Admin")
+        Admin = Group.objects.create(name="Admin")
+        # perms=['13','14','16','8','5','6','9','10','12']
+        # for p in perms:
+        #     Admin.permissions.add(p)
+        Senior_Admin = Group.objects.create(name="Senior_Admin")
         perms2=['1','2','4','5','6','8','9','10','12','13','14','16','17','18','19','21','22','23','24']
         for p2 in perms2:
-            Non_SU_Admin.permissions.add(p2)
+            Senior_Admin.permissions.add(p2)
         #password fine in here as after first log on required to change it
         user = User.objects.create_user("Admin", "", "password")
         user.first_name = ""
