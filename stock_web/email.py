a28631806625fffe0e48eb9e6f2027c7ceccfbe2
Kieran Howard <kieran.howard@ouh.nhs.uk>
2020-10-15 12:38:59 +0000

    Changes to email HTML formatting

--- stock_web/email.py
+++ stock_web/email.py
@@ -28,7 +28,7 @@ def send(sbj,text,to):
     msg["From"]="ORGLStock@gmail.com"
     msg["Subject"]=sbj
     msg.add_header('reply-to', reply_to)
-    text+="You can visit the site at: {}<br><br>".format(settings.SITE_URL)
+    text+="You can visit the site at: <a href='{0}'> {0} </a><br><br>".format(settings.SITE_URL)
     text+="This message was automatically sent on behalf of the Stock Team.<br><br>"
     #pdb.set_trace()
     html='<html><head></head><body>{}If you have any issues please contact <a href="mailto:Kieran.howard@ouh.nhs.uk?subject=Stock%20Database">kieran.howard@ouh.nhs.uk</a> or reply to this message.</p></body></html>'.format(text)
