8df02c86ca7b69d99c3d0fe27331d5cc9a1a614e
Kieran Howard <kieran.howard@ouh.nhs.uk>
2021-02-02 17:15:43 +0000

    Volumes get correctly subtrated from solution components
    39c8d27ec0f3f4d83ef13c62c86005cd047fab2a changed it so that non volume 
    tracked solutions can have volume tracked components. However the 
    section of code that "used" the volumes for components still assumed the 
    solution had it's volume tracked, this fixes that

--- stock_web/models.py
+++ stock_web/models.py
@@ -493,8 +493,8 @@ class Solutions(models.Model):
                 values["num_rec"]=1
             else:
                 values["vol_rec"]=vol_made
-                for item, vol in vols_used.items():
-                    Inventory.take_out(vol,item,user,sol=solution)
+            for item, vol in vols_used.items():
+                Inventory.take_out(vol,item,user,sol=solution)
             return Inventory.create(values,user), changed, EXP_DATE
     def list_comp(self):
         comps=[]
