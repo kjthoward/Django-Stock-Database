f3f375c655c494c1c633cffecba1fbac86bd0750
Kieran Howard <kieran.howard@ouh.nhs.uk>
2021-08-17 14:07:54 +0100

    Formatting for previous commit
    4c33e296ec0be79f8b9b030f17f35389bcf3f0c2 didn't have Black run over it 
    for consistent formatting

--- stock_web/views.py
+++ stock_web/views.py
@@ -133,16 +133,16 @@ def vol_migrate(httprequest):
     for reagent in all_reagents:
         num_open = open_items.filter(reagent=reagent)
         num_unopen = un_open_items.filter(reagent=reagent)
-        if reagent.track_vol==False:
+        if reagent.track_vol == False:
             reagent.open_no = num_open.count()
             reagent.count_no = num_unopen.count()
             reagent.save()
         else:
-            vol=0
+            vol = 0
             for item in num_unopen:
-                vol+=item.vol_rec
+                vol += item.vol_rec
             for item in num_open:
-                vol+=item.current_vol   
+                vol += item.current_vol
             reagent.open_no = num_open.count()
             reagent.count_no = vol
             reagent.save()
